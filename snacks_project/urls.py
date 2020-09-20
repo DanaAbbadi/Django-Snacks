a0aaffdf359d93833654e74d186ccde190a65419
Dana Al-Abbade <abbadidana91@gmail.com>
2020-09-20 23:44:18 +0300

    added chocolate

--- snacks_project/urls.py
+++ snacks_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snacks.urls')),
+    path('chocolate/', include('chocolate.urls')),
 ]
